8301eb1d6335f51ea68137f270fb0c7e89867bef
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-04-05 10:59:22 +0200

    Add function to store new books and fill array with some test data

--- script/script.js
+++ script/script.js
@@ -8,4 +8,18 @@ function Book(title, author, pages, read) {
     this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
+addBookToLibrary("The Hobbit 2", "J.R.R Tolkien", 300, "not read yet");
+addBookToLibrary("The Hobbit 3", "J.R.R Tolkien", 310, "not read yet");
+addBookToLibrary("Harry Potter 1", "J.K Rowling", 200, "read");
+addBookToLibrary("Harry Potter 2", "J.K Rowling", 250, "read");
+addBookToLibrary("Harry Potter 1", "J.K Rowling", 300, "not read yet");
+
+console.log(myLibrary);
+
 
